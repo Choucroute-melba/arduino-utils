329385ec777288b17e85fb486e12a273d6e073b0
Vivien <vivien@netc.fr>
2022-06-03 19:19:28 +0200

    allow to programatically create commands

--- CommandLine.h
+++ CommandLine.h
@@ -10,16 +10,29 @@ class Command
   char * getValue(const char * argName);
 
   char * name() { return m_cmdName; }
+  bool setName(const char * newName);
   char * args() { return m_rawArgs; }
+  bool setArg(const char * newArg);
   char * raw() { return m_rawCmd; }
+  bool setRaw(const char * newRaw);
 
   private :
+  bool parse();
+
   char * m_rawCmd;
   int m_rawCmdLength;
+  char * m_newRawCmd;
+  int m_newRawCmdLength;
+
   char * m_cmdName;
   int m_cmdNameLength;
+  char * m_newCmdName;
+  int m_newCmdNameLength;
+
   char * m_rawArgs;
   int m_rawArgsLength;
+  char * m_newRawArgs;
+  int m_newRawArgsLength;
 };
 
 
